1ecd247a8cf4d87e56370b14a0b2a8947682cd73
Anurag <anuragchandra1601@gmail.com>
2024-07-13 02:42:32 +0530

    fourth commit

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.log(err));
 
   const corsOptions = {
-    origin: ["http://localhost:5173", "*"],
+    origin: ["https://resumebuilder-u7xz.vercel.app/", "*"],
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify the methods you want to allow
     credentials: true // Allow cookies to be sent with requests
 };
