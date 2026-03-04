f88b625176bcaaeaf9feff647e9d1ea5ce929dff
Anurag <anuragchandra1601@gmail.com>
2026-03-04 23:39:26 +0530

    fix tailwind setup

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-// module.exports = {
-//     plugins: {
-//       tailwindcss: {},
-//       autoprefixer: {},
-//     },
-//   }
-
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
-=======
-// export default {
-//   plugins: {
-//     "@tailwindcss/postcss": {},
-//     autoprefixer: {},
-//   },
-// }
->>>>>>> 14d979e (fix tailwind postcss plugin)
+};
